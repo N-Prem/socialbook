cbcc4692c23e504121362476b15dea029f70678d
Nayantara Prem <tara.prem111@gmail.com>
2013-07-15 11:59:15 -0400

    Created statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialbook::Application.config.secret_token = 'c42e58c083194ad8ad045ec139e44fa9e41e3c7eff823903ae4faaa1fa9ca4f4ea01ad9d323291d173a723b26e6e2272db2f8a4692ae6d357fbc863ad2bff87a'
